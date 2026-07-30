01c8c37e3055b068bbef37d53fa932f4d007b2dd
Ava Allwardt <89409277+avaallwardt@users.noreply.github.com>
2026-07-30 19:15:17 +0000

    Added 2 pointers solution to bucket-list

--- bucket-list.py
+++ bucket-list.py
@@ -1,3 +1,10 @@
+'''CORRECT: both method 1 (num items in list at any given time) and method 2 (prefix sums)'''
+
+'''Method 1:
+- separate lists for start times, end times, and buckets
+- create a new list holding number buckets needed at any given time
+'''
+
 num_cows = int(input())
 
 # create lists to store the start times, end times, and buckets needed for each of the num_cows cows
@@ -22,4 +29,26 @@ ans = max(slots)
 
 print(ans)
 
+
+''' Method 2
+- prefix sum
+- build slots list as take in input about the n cows
+'''
+
+n = int(input())
+slots = [0] * 1005 # make it > 1005 so we 
+for _ in range(n):
+    s, t, b = map(int, input().split())
+    slots[s] += b
+    slots[t+1] -= b # now the prefix sum from s to t will include b then b will be subtracted after t
+    # takes less time than putting p in all slots from s to t because only place b in slots s and t-1 (neg b) (2 total slots) rather than all slots s to t (t-s+1 total slots)
+
+# calculate max prefix sum in slots list
+total = 0
+ans = 0
+for i in range(1005):
+    total += slots[i] # total represents the current prefix sum from index 0 to i
+    ans = max(ans, total)
+
+print(ans)
 
